1cbb7d1bfa4ced8a5e5d67d5f703885015bd7c42
Marcus Bello <marcusthetechguru@gmail.com>
2025-11-03 20:33:49 -0600

    role-based dashboard

--- MrIgor.Mvc/Controllers/DashboardController.cs
+++ MrIgor.Mvc/Controllers/DashboardController.cs
@@ -6,11 +6,38 @@ namespace MrIgor.Mvc.Controllers
     public class DashboardController : Controller
     {
         // GET: DashboardController
-        [Authorize(Roles = "Admin, Teacher, Student")]
+        [Authorize(Roles = "SuperAdmin, Admin, Teacher, Student")]
         public ActionResult Index()
         {
             return View();
         }
 
+        // GET: DashboardController/SuperAdmin
+        [Authorize(Roles = "SuperAdmin")]
+        public ActionResult SuperAdmin()
+        {
+            return View();
+        }
+
+        // GET: DashboardController/Admin
+        [Authorize(Roles = "Admin")]
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+        // GET: DashboardController/Teacher
+        [Authorize(Roles = "Teacher")]
+        public ActionResult Teacher()
+        {
+            return View();
+        }
+
+        // GET: DashboardController/Student
+        [Authorize(Roles = "Student")]
+        public ActionResult Student()
+        {
+            return View();
+        }
     }
 }
